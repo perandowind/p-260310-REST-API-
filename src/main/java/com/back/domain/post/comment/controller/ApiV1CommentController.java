917bb30e60b684b4bdb33c14d19951a6bf23aa10
perandowind <perandowind@gmail.com>
2026-03-11 12:12:55 +0900

    16

--- src/main/java/com/back/domain/post/comment/controller/ApiV1CommentController.java
+++ src/main/java/com/back/domain/post/comment/controller/ApiV1CommentController.java
@@ -54,11 +54,13 @@ public class ApiV1CommentController {
             @PathVariable int commentId
     ) {
         Post post = postService.findById(postId).get();
+        Comment comment = post.findCommentById(commentId).get();
         post.deleteComment(commentId);
 
         return new RsData(
                 "%d번 댓글이 삭제되었습니다.".formatted(commentId),
-                "204-1"
+                "204-1",
+                new CommentDto(comment)
         );
     }
 }
