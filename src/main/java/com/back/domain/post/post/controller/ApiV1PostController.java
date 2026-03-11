945bce4e180f0ac2c3439825879310635101abfd
perandowind <perandowind@gmail.com>
2026-03-11 15:48:29 +0900

    25

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -8,7 +8,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -52,11 +51,11 @@ public class ApiV1PostController {
     ){}
 
     @PostMapping
-    public ResponseEntity<RsData<PostWriteResBody>> write(@RequestBody @Valid PostWriteReqBody reqBody) {
+    public RsData<PostWriteResBody> write(@RequestBody @Valid PostWriteReqBody reqBody) {
         Post post = postService.write(reqBody.title, reqBody.content);
         long postsCount = postService.count();
 
-        RsData<PostWriteResBody> rsData = new RsData<>(
+        return new RsData<>(
                 "%d번 글이 성공적으로 작성되었습니다.".formatted(post.getId()),
                 "201-1",
                 new PostWriteResBody(
@@ -64,7 +63,7 @@ public class ApiV1PostController {
                         postsCount
                 )
         );
-        return ResponseEntity.status(201).body(rsData);
+
     }
 
     @DeleteMapping("/{id}")
@@ -76,9 +75,9 @@ public class ApiV1PostController {
 
         return new RsData<>(
                 "%d번 댓글이 삭제되었습니다.".formatted(id),
-                "204-1",
-                new PostDto(post)
+                "204-1"
         );
+
     }
 
 }
