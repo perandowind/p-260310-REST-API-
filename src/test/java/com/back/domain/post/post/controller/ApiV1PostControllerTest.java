6f3f46114ea3a29c3ca8b2c2f57e71393c573612
perandowind <perandowind@gmail.com>
2026-03-13 11:27:15 +0900

    43, 44, 45

--- src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -54,7 +54,7 @@ public class ApiV1PostControllerTest {
     }
 
     @Test
-    @DisplayName("글 단건 조회")
+    @DisplayName("글 단건 조회 - 성공")
     void t2() throws Exception {
 
         int targetId = 1;
@@ -87,8 +87,26 @@ public class ApiV1PostControllerTest {
     }
 
     @Test
-    @DisplayName("글 작성")
+    @DisplayName("글 단건 조회, 존재하지 않는 글")
     void t3() throws Exception {
+        int targetId = Integer.MAX_VALUE;
+
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/posts/%d".formatted(targetId))
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("detail"))
+                .andExpect(status().isNotFound()); // 404
+
+    }
+
+    @Test
+    @DisplayName("글 작성")
+    void t4() throws Exception {
         String title = "제목입니다";
         String content = "내용입니다";
 
@@ -119,8 +137,36 @@ public class ApiV1PostControllerTest {
     }
 
     @Test
+    @DisplayName("글 작성, 제목이 입력되지 않은 경우")
+    void t5() throws Exception {
+        String title = "";
+        String content = "내용입니다";
+
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "title": "%s",
+                                            "content": "%s"
+                                        }
+                                        """.formatted(title, content))
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("write"))
+                .andExpect(status().isBadRequest()) // 400
+                .andExpect(jsonPath("$.resultCode").value("400-1"))
+                .andExpect(jsonPath("$.msg").value("title-NotBlank-01-title-제목은 필수입니다.\ntitle-Size-03-title-제목은 2자 이상 10자 이하로 입력해주세요."));
+
+    }
+
+    @Test
     @DisplayName("글 수정")
-    void t4() throws Exception {
+    void t6() throws Exception {
         int targetId = 1;
         String title = "제목 수정";
         String content = "내용 수정";
@@ -155,7 +201,7 @@ public class ApiV1PostControllerTest {
 
     @Test
     @DisplayName("글 삭제")
-    void t5() throws Exception {
+    void t7() throws Exception {
         int targetId = 1;
 
         ResultActions resultActions = mvc
